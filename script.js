bd28d2817e1e1af298610e6189ec62d0f5f32197
Jadis96 <128143470+Jadis96@users.noreply.github.com>
2023-07-22 17:42:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+
+
+
+
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random()*3) + 1;
+    let randomString;
+    switch (randomNumber) {
+        case 1:
+            randomString = "Rock";
+            break;
+        case 2:
+            randomString = "Paper";
+            break
+        case 3:
+            randomString = "Scissors"; 
+            break;
+        default:
+            randomString = " ";
+            break;
+    }
+    return randomString;
+}
+
+
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase(); 
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        let specificMessage;
+         if (playerSelection === "rock" && computerSelection === "rock") {
+        specificMessage = "Rock ties with rock.";
+        } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        specificMessage = "Scissors tie with scissors.";
+        } else if (playerSelection === "paper" && computerSelection === "paper") {
+         specificMessage = "Paper ties with paper.";
+        }
+        return specificMessage + " " + "It's a tie!";
+    } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock") 
+    
+    )
+    { 
+        let specificMessage;
+        if (playerSelection === "rock" && computerSelection === "scissors") {
+            specificMessage = "Rock beats scissors.";
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            specificMessage = "Scissors beats paper.";
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            specificMessage = "Paper beats rock.";
+        }
+        return specificMessage + " " + "You win!";
+    } else {
+    return "You lose";
+    }
+
+} 
+
+
+
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+    console.log("Welcome! Let's begin our game")
+    
+
+    for (let round = 1; round <= 5; round++) {
+        console.log("Round " + round);
+
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection)
+
+    const playerSelection = prompt("Enter your choice: Paper, Rock or Scissors");
+    console.log(playerSelection);
+        let roundResult = playRound (playerSelection, computerSelection);
+        console.log(roundResult)
+
+        if (roundResult.includes ("It's a tie!")) {
+            playerScore++;
+            computerScore++;
+        } else if (roundResult.includes ("You win!")) {
+            playerScore++
+        } else {
+            computerScore++
+        }
+        
+        console.log("The score is: ");
+        console.log("Your score: " + playerScore);
+        console.log("Computer's score: " + computerScore);
+
+    }
+}
+ 
+game() 
